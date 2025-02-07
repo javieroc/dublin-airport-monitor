9c60a1e68fdb9df20f5f05496f108d57086c5eeb
Javier Ocampo <ferroxido@gmail.com>
2025-02-07 20:13:06 +0000

    styles: favicon

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,7 +6,7 @@ const Navbar: FC = () => {
   const {fancyTimetableDate, previousDate, nextDate} = useTimetableDate();
   return (
     <nav className="flex flex-col items-center gap-2 border-b border-gray-200 bg-gray-50 p-4">
-      <h1 className="text-3xl font-bold text-gray-800">Ryan-Fail</h1>
+      <h1 className="text-3xl font-bold text-gray-800">RyanFail</h1>
       <menu className="flex content-center gap-2">
         <button onClick={previousDate} className="flex h-8 w-8 items-center justify-center rounded-md bg-gray-200 hover:bg-gray-300 active:bg-gray-400" aria-label="Left">
           <FaArrowLeft className="size-4 text-gray-800" />
